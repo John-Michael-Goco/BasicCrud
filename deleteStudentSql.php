52fab09b739877864537e7124f7f69b7ea33b94f
Dela Pena Aero Kenn Gulapa <aerogulapa1423@gmail.com>
2025-01-31 09:40:28 +0800

    Finish Delete Student

--- deleteStudentSql.php
+++ deleteStudentSql.php
@@ -1,21 +1,19 @@
 <?php
-include '/includes/dbconn.php';
+include 'includes/dbconn.php';
 
 $studentID = $_GET['studentID'];
 
-$newsID = mysqli_real_escape_string($conn, $agentID);
-
-$sql = "UPDATE `agents` SET agentStatus = 'Inactive' WHERE agentID = '$agentID'";
+$sql = "DELETE FROM `students-info` WHERE studentID = '$studentID'";
 
 $result = mysqli_query($conn, $sql);
 
 if ($result) {
   // Successful deletion
-  echo '<script>alert("News deleted successfully!");</script>';
+  echo '<script>alert("Student deleted successfully!");</script>';
 } else {
   // Deletion failed
-  echo '<script>alert("Failed to delete news!");</script>';
+  echo '<script>alert("Failed to delete student info!");</script>';
 }
 
-echo '<script>setTimeout(function(){ window.location.href = "../Agents.php"; }, 200);</script>';
+echo '<script>setTimeout(function(){ window.location.href = "index.php"; }, 200);</script>';
 exit(); // Make sure to exit after redirect
